82463db77a63ed1a3e315048c4ff7014d66735c0
VENKTESH KUMAR MISRA <venkteshkumarmisra8181@gmail.com>
2022-04-06 12:42:31 +0530

    add multer library for adding pictures as a data

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -12,13 +12,64 @@
                 data:newPostForm.serialize(),
                 success:function(data)
                 {
-                    console.log(data);
+                let newPost=newPostDom(data.data.post);
+                $('#posts-list-container>ul').prepend(newPost);
+                deletePost($(' .delete-post-button',newPost));
                 },error:function(error){
                     console.log(error.responseText);
                 }
             });
         });
     }
-    // method to create a post in the dom
+        // method to create a post for data
+        let newPostDom=function(post)
+        {
+            return $(`<li id="post-${post._id}">
+            <p>
+                
+                <small>
+               <a class="delete-post-button" href="/posts/destroy/${post._id}"> x</a>
+                </small>
+                
+        ${post.content}
+        <br>
+        <small>
+        ${post.user.name}
+        </small>
+        </p>
+        <div class="post-comments">
+            <form action="/comments/create" method="POST">
+                <input type="text" name="content" placeholder="Type here to add comment" required>
+                <input type="hidden" name="post" value="${post._id}">
+                <input type="submit" value="add-comment">
+            
+            </form>
+        
+         <div class="post-comments-list">
+             <ul id="post-comments-${post._id}">
+        
+             </ul>
+            
+         </div>
+        </div>
+        </li>`)
+        }
+
+    let deletePost=function(deleteLink)
+    {
+        $(deleteLink).click(function(e){
+            e.preventDefault();
+            $.ajax({
+                type:'get',
+                url:$(deleteLink).prop('href'),
+                success:function(data){
+                  $(`#post-${data.data.post_id}`).remove();
+                },error:function(){
+                    console.log(error.responseText);
+                }
+            })
+        })
+    }
+    
     createPost();
 }
--- controller/posts_controller.js
+++ controller/posts_controller.js
@@ -34,6 +34,15 @@ module.exports.destroy= async function(req,res)
         {
             post.remove();
            await Comment.deleteMany({post:req.params.id});
+            if(req.xhr)
+            {
+                return res.status(200).json({
+                    data:{
+                       post_id:req.params.id 
+                    },
+                    message:"Post deleyed succesfully"
+                })
+            }
                 req.flash('success','Post and associated comment are deleted');
                 return res.redirect('back');   
         }
